1b91483e95c35e99d895485892399a7cfe8cdd7b
Atharva Tarlekar <atharvatarlekar162001@gmail.com>
2023-04-18 12:46:12 +0530

    admin login

--- web-admin/pages/index.js
+++ web-admin/pages/index.js
@@ -1,86 +1,104 @@
-import { async } from "@firebase/util";
-import { useContext, useEffect, useState } from "react";
-import SideNavbar from "../components/snavbar";
-// import { AppContext } from "../../../context/AppContext";
-// import { getAuth, onAuthStateChanged } from "firebase/auth";
+import React from "react";
+import { useState, useEffect } from 'react';
 import { db } from "../firebase-config";
-import { getDocs, collection } from "@firebase/firestore";
-const Sdashboard = () => {
-    const [weekday, setWeekday] = useState('');
+import { collection, getDocs } from "@firebase/firestore"
+import Link from 'next/link';
+import {Router,useRouter} from "next/router";
+const Login = () => {
+  
+  const router = useRouter();
+  const userCollectionRef = collection(db, "admins");
+  const [users, setUsers] = useState([]);
+  // const [currusername, setcurrentusername] = useState([]);
+  // const [currpass, setcurrpass] = useState([]);
 
-    useEffect(() => {
-        const today = new Date();
-        const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-        const weekday = daysOfWeek[today.getDay()];
-        setWeekday(weekday);
-    }, []);
-    const [users, setUsers] = useState([]);
-    const userCollectionRef = collection(db, "Messmenu");
-    useEffect(() => {
-        const getUsers = async () => {
-            const data = await getDocs(userCollectionRef); 
-            setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-        };
-        getUsers();
-    }, []);
-    const [breakfast, setbreakfast] = useState('');
-    const [lunch, setlunch] = useState('');
-    const [dinner, setdinner] = useState('');
-    const handlemessmenu = (user) => {
-        setbreakfast(user.Breakfast);
-        setlunch(user.Lunch);
-        setdinner(user.Dinner);
+  const userFound = () => {
+    alert("Logged in Successfully");
+    router.push("/admin");
+  };
+  const userNotFound = () => {
+    alert("You have entered wrong Email or Password");
+    router.push("/");
+  };
+
+  const handleClickLogIn=(e)=>{
+    var count = 0;
+    e.preventDefault();
+    const currname=e.target.name.value;
+    const currpass=e.target.password.value;
+    var userFound1 = false;
+    console.log(users);
+    users.map((user) => {
+      user.username === currname && user.password === currpass
+        ? (userFound1 = true)
+        : (count = count + 1);
+    });
+
+    {
+      userFound1 === true ? userFound() : userNotFound();
     }
-    useEffect(() => {
-        users.map((user) => {
-            user.Day === weekday ? (handlemessmenu(user)) : (console.log('hello'));
-        })
-    })
-    return (
-        <div className="lg:ml-52 md:ml-12">
-            <div className="py-5"></div>
-            <SideNavbar />
-            <div class="messmenuwrapper">
-                <ul class="messmenuflex messmenucards">
-                    <li class="rounded overflow-hidden shadow-lg">
-                        <div class="px-6 py-4">
-                            <div class="font-bold text-xl mb-2">Student&apos;s count</div>
-                            <p class="text-gray-700 text-base">
-                                500
-                            </p>
-                        </div>
-                    </li>
-                    <li class="rounded overflow-hidden shadow-lg">
-                        <div class="px-6 py-4">
-                            <div class="font-bold text-xl mb-2">{weekday}&apos;s Breakfast</div>
-                            <p class="text-gray-700 text-base">
-                                {breakfast}
-                            </p>
-                        </div>
 
-                    </li>
-                    <li class="rounded overflow-hidden shadow-lg">
-                        <div class="px-6 py-4">
-                            <div class="font-bold text-xl mb-2">{weekday}&apos;s Lunch</div>
-                            <p class="text-gray-700 text-base">
-                                {lunch}
-                            </p>
-                        </div>
+  };
 
-                    </li>
-                    <li class="rounded overflow-hidden shadow-lg">
-                        <div class="px-6 py-4">
-                            <div class="font-bold text-xl mb-2">{weekday}&apos;s Dinner</div>
-                            <p class="text-gray-700 text-base">
-                                {dinner}
-                            </p>
-                        </div>
-                    </li>
-                </ul>
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(userCollectionRef);
+      // console.log(data);
+      // setUsers(data);
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
 
-            </div>
-        </div>
-    );
-}
+    getUsers();
+  }, []);
 
-export default Sdashboard;
+  return (
+    <div className="mt-20 ">
+      <div className="container ">
+        <h2 class="login-title">Admin</h2>
+
+        <form class="login-form" onSubmit={(e) => {handleClickLogIn(e)}}>
+          <div>
+            <label for="name">Username</label>
+            <input
+              id="name"
+              type="text"
+              placeholder="Admin username"
+              name="name"
+              required
+              // onChange={(e)=>{setcurrentusername(e.target.value)}}
+            />
+          </div>
+
+          {/* <div>
+            <label for="email">Email </label>
+            <input
+              id="email"
+              type="email"
+              placeholder="me@example.com"
+              name="email"
+              required
+            />
+          </div> */}
+
+          <div>
+            <label for="password">Password </label>
+            <input
+              id="password"
+              type="password"
+              placeholder="password"
+              name="password"
+              required
+              // onChange={(e)=>{setcurrpass(e.target.value)}}
+            />
+          </div>
+
+          <button class="btn btn--form" type="submit" value="Log in">
+            Log in
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
