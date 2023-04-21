1ee52a9b45f28cc6ef6859e472bc9413eb4a0b14
Yash Chindhe <yashschindhe@gmail.com>
2023-04-22 00:25:46 +0530

    snavbar changed to topnavbar

--- web-admin/components/snavbar.js
+++ web-admin/components/snavbar.js
@@ -22,51 +22,45 @@ function SideNavbar() {
    return (
       <div className="s-layout">
          {/* <!-- Sidebar --> */}
-         <div className="s-layout__sidebar">
-            <a className="s-sidebar__trigger" href="#0">
-               <i className="fa fa-bars"></i>
-            </a>
-
-            <nav className="s-sidebar__nav">
-               <ul>
-                  <li>
-                     <Link className="s-sidebar__nav-link" href='/'>
-                        <i className="fa fa-home"></i><em>Dashboard</em>
-                     </Link>
-                  </li>
-                  <li>
-                     <Link className="s-sidebar__nav-link" href="/admin/notifications">
-                        <i className="fa fa-bell"></i><em>Notifications</em>
-                     </Link>
-                  </li>
-                  <li>
-                     <Link className="s-sidebar__nav-link" href="/admin/announcements">
-                        <i className="fa fa-bullhorn"></i><em>Announcements</em>
-                     </Link>
-                  </li>
-                  <li>
-                     <Link className="s-sidebar__nav-link" href="/admin/messmenu">
-                        <i className="fa fa-cutlery"></i><em>Mess Menu</em>
-                     </Link>
-                  </li>
-                  <li>
-                     <Link className="s-sidebar__nav-link" href="/admin/verification">
-                        <i class="fa fa-check" aria-hidden="true"></i><em>Verification</em>
-                     </Link>
-                  </li>
-                  <li>
-                     <Link className="s-sidebar__nav-link" href="/admin/allstudents">
-                        <i className="fa fa-male"></i><em>Students</em>
-                     </Link>
-                  </li>
-                  <li>
-                     <Link className="s-sidebar__nav-link" href="/admin/reminder">
-                        <i className="fa fa-male"></i><em>Reminder</em>
-                     </Link>
-                  </li>
-               </ul>
-            </nav>
-         </div>
+         <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+            <div class="flex items-center flex-shrink-0 text-white mr-6">
+               <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" /></svg>
+               <span class="font-semibold text-xl tracking-tight">VJTI Mess</span>
+            </div>
+            <div class="block lg:hidden">
+               <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+                  <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
+               </button>
+            </div>
+            <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+               <div class="text-sm lg:flex-grow">
+                  <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                     Dashboard
+                  </a>
+                  <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                     Announcements
+                  </a>
+                  <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+                     Notifications
+                  </a>
+                  <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+                     Reminders
+                  </a>
+                  <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+                      QR Scanner
+                  </a>
+                  <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+                     Verifications
+                  </a>
+                  <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+                     Mess Menu
+                  </a>
+                  <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+                     Inventory
+                  </a>
+               </div>
+            </div>
+         </nav>
       </div>
    )
 }
