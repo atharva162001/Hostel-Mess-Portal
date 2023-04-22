bc22ea904c5ad95a6e1e973fc84227e730523997
Akshay <afalke007@gmail.com>
2023-04-23 03:45:43 +0530

    add navbar design  and navbar routing

--- web-admin/components/login/PleaseLog.jsx
+++ web-admin/components/login/PleaseLog.jsx
@@ -7,7 +7,7 @@ function Heading() {
 
         const timeout = setTimeout(() => {
             router.push('/');
-          }, 1000); // 1 seconds
+          }, 100); // 1 seconds
          
           return () => clearTimeout(timeout); // clean up the timeout on unmount  
 
@@ -15,8 +15,8 @@ function Heading() {
     
   return (
     <div>
-        <h1 className='dark:text-white font-sans font-bold text-3xl sm:font-extrabold sm:text-4xl pt-28 text-center '>Please Log In to access portal.</h1>
-        <h1 className='dark:text-white font-sans font-bold text-2xl sm:font-bold sm:text-3xl pt-28 text-center '>You are being redirected!!</h1>
+        {/* <h1 className='dark:text-white font-sans font-bold text-3xl sm:font-extrabold sm:text-4xl pt-28 text-center '>Please Log In to access portal.</h1> */}
+        {/* <h1 className='dark:text-white font-sans font-bold text-2xl sm:font-bold sm:text-3xl pt-28 text-center '>You are being redirected!!</h1> */}
     </div>
   )
 }
