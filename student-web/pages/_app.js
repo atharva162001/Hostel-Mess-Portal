80751ac1379b6a2358d3caaa53de51d0b93dd9dd
Samarth <splangote_b20@ce.vjti.ac.in>
2023-04-23 15:10:25 +0530

    navbar changed 2

--- student-web/pages/_app.js
+++ student-web/pages/_app.js
@@ -1,5 +1,10 @@
 import '@/styles/globals.css'
-
+import Navbar from '../components/navbar'
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+  <Navbar links={["Home", "Projects", "Articles", "About"]} ctaText="Contact"/>;
+    <Component {...pageProps} />
+    </>
+  )
 }
